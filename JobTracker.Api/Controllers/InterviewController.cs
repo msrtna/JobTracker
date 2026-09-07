0eef6be02ba581be2e161a9af0b4fc4e1c881da7
msrtna <mohalimor64@gmail.com>
2026-09-07 04:59:14 +0330

    Add controller. next step is refactor Interview payroll

---
+++ JobTracker.Api/Controllers/InterviewController.cs
@@ -0,0 +1,57 @@
+﻿using JobTracker.Application.DTOs.InterviewDtos;
+using JobTracker.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobTracker.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InterviewController : ControllerBase
+    {
+        private readonly IInterviewService _service;
+
+        public InterviewController(IInterviewService service)
+        {
+            _service = service;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await _service.GetAllAsync();
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(long id)
+        {
+            var result = await _service.GetByIdAsync(id);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync([FromBody] CreateInterviewDto dto)
+        {
+            var result = await _service.CreateAsync(dto);
+            return CreatedAtAction(
+                        nameof(GetById),
+                        new { id = result.Id },
+                        result);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateAsync(long id, [FromBody] UpdateInterviewDto dto)
+        {
+            await _service.UpdateAsync(id, dto);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAsync(long id)
+        {
+            await _service.DeleteAsync(id);
+            return NoContent();
+        }
+    }
+}
