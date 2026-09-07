b954406af696bac421a08e0ddbcd03972716b8e6
msrtna <mohalimor64@gmail.com>
2026-09-07 03:50:23 +0330

    Add controller and test swager successfully

--- JobTracker.Api/Controllers/JobCategoryController.cs
+++ JobTracker.Api/Controllers/JobCategoryController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using JobTracker.Application.DTOs.JobCategoryDtos;
+using JobTracker.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace JobTracker.Api.Controllers
 {
@@ -6,6 +8,50 @@ namespace JobTracker.Api.Controllers
     [ApiController]
     public class JobCategoryController : ControllerBase
     {
+        private readonly IJobCategoryService _service;
 
+        public JobCategoryController(IJobCategoryService service)
+        {
+            _service = service;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await _service.GetAllAsync();
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(long id)
+        {
+            var result = await _service.GetByIdAsync(id);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync([FromBody] CreateJobCategoryDto dto)
+        {
+            var result = await _service.CreateAsync(dto);
+            return CreatedAtAction(
+                        nameof(GetById),
+                        new { id = result.Id },
+                        result);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateAsync(long id, [FromBody] UpdateJobCategoryDto dto)
+        {
+            await _service.UpdateAsync(id, dto);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAsync(long id)
+        {
+            await _service.DeleteAsync(id);
+            return NoContent();
+        }
     }
 }
