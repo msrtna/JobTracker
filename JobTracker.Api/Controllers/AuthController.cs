db4ab0e17c67eeca25559bc111b7c70bb5009f7f
msrtna <mohalimor64@gmail.com>
2026-09-10 07:40:54 +0330

    Add controller

---
+++ JobTracker.Api/Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using JobTracker.Application.DTOs.AuthDtos.RegisterDtos;
+using JobTracker.Application.DTOs.AuthDtos.UserDtos;
+using JobTracker.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobTracker.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public AuthController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+
+        [HttpPost("register")]
+        public async Task<ActionResult<UserDto>> Register(RegisterDto dto)
+        {
+            var result = await _userService.RegisterAsync(dto);
+
+            return Ok(result);
+        }
+    }
+}
